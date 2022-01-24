7acd6b80a8dc96ab54be4c949049866c3e1d38b1
lucasinchains <vitantoniolucas@gmail.com>
2022-01-24 16:25:36 +0100

    implementing useEffect to call cryptocompare api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useState } from "react"
 import Form from "./components/Form"
 import VideoList from "./components/VideoList"
 import Error from "./components/Error"
+import axios from "axios"
+
 
 function App() {
   const [currency, setCurrency] = useState('');
   const [cryptocurrency, setCryptocurrency] = useState('');
   const [currencyPair, setCurrencyPair] = useState([]);
   const [isValidForm, setIsValidForm] = useState(true);
+
+  useEffect(async () => {
+    const response = await axios.get({url: 'https://min-api.cryptocompare.com/data/pricemulti', config: {
+      fsyms: `${cryptocurrency}`,
+      tsyms: `${currency}`,
+      api_key: 'b4d53e80e4c387bd80c71b530ce5a14a273bec1d99a88fa04907cdf1c1b00ade'
+    }}
+    )
+      console.log(response)
+    }, [currencyPair]);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +29,6 @@ function App() {
       setIsValidForm(false);
       return;
     } else { 
-      setIsValidForm(true);
       setCurrencyPair([currency, cryptocurrency]);
     }
   }; 
