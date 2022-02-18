25880089c7475fe854c7ef97fbcbb01a75dd5cfa
lucasinchains <vitantoniolucas@gmail.com>
2022-02-18 18:26:57 +0000

    refactoring

--- src/components/DataInfo.jsx
+++ src/components/DataInfo.jsx
@@ -2,28 +2,17 @@ import React from 'react';
 
 const DataInfo = ({data}) => {
 
-    const dataObject = {
-        price: data[0]['PRICE'],
-        dayHigh: data[0]['HIGHDAY'],
-        dayLow: data[0]['LOWDAY'],
-        dayChange: data[0]['CHANGE24HOUR']
-    };
-
-    const {price, dayHigh, dayLow, dayChange} = dataObject;
+    const dataInfo = [
+        {price: data[0]['PRICE'], tag: 'Price'},
+        {price: data[0]['HIGHDAY'], tag: 'Day High'},
+        {price: data[0]['LOWDAY'], tag: 'Day Low'},
+        {price: data[0]['CHANGE24HOUR'], tag: 'Day Change'}
+    ];
 
   return (
       <div className="data-info">
         <div className="data-info-item">
-          <p>Price: <span>{price}</span></p>
-        </div>
-        <div className="data-info-item">
-          <p>Day high: <span>{dayHigh}</span></p>
-        </div>
-        <div className="data-info-item">
-          <p>Day low: <span>{dayLow}</span></p>
-        </div>
-        <div className="data-info-item">
-          <p>Change last 24H: <span>{dayChange}</span></p>
+          {dataInfo.map(infoItem => <p>{infoItem.tag}: <span>{infoItem.price}</span></p>)}
         </div>
       </div>
   );
